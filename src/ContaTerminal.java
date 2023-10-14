fd1eba829031fc29690e915ed28143bf6b7c87d2
walter dev reis <walter-reis@outlook.com>
2023-10-14 10:34:21 -0300

    Adicionar comentarios com TODO: para comentar as etapas do projeto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,12 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //TODO: Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
     }
 }
