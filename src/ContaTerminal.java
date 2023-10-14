4958b6d952fb238e414b49a0cc65aabe0340ba10
walter dev reis <walter-reis@outlook.com>
2023-10-14 10:51:15 -0300

    Trabalhando e importando a classe Scanner

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,9 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         
         //TODO: Conhecer e importar a classe Scanner
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        //TODO: Criando os atributos da conta
+ 
 
         //Exibir as mensagens para o nosso usuário
+
 
         //Obter pela scanner os valores digitados no terminal
 
