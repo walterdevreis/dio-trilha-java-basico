f9364b3c02585ceb73a3453d35902e483de60448
walter dev reis <walter-reis@outlook.com>
2023-10-14 11:08:34 -0300

    Criado os atributos da Conta

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -8,10 +8,14 @@ public class ContaTerminal {
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
 
         //TODO: Criando os atributos da conta
-        int numero;
-        String agencia;
-        String nomeCliente;
-        double saldo;
+        System.out.print("Por favor, isira os dados do novo Cliente: ");
+        int numero = Integer.valueOf(args[0]);
+        System.out.print("Por favor, digite o número da Agência! ");
+        String agencia = args[1];
+        System.out.print("Por favor, digite o nome do cliente! ");
+        String nomeCliente = args[2];
+        System.out.print("Por favor, digite o saldo inicial da conta! ");
+        double saldo = Double.valueOf(args[3]);
 
         //Exibir as mensagens para o nosso usuário
 
